d1dceccd3602ec28135478374741f8e3f76b796f
Jose Antonio Paiva <japaiva@gmail.com>
2025-05-27 11:06:16 -0300

    Ok docker

--- core/services/__init__.py
+++ core/services/__init__.py
@@ -1,6 +1,7 @@
+
 from .calculo_cabine import CalculoCabineService
 from .calculo_carrinho import CalculoCarrinhoService
-from .calculo_pedido import CalculoPedidoService, ComponenteCalculoService
+from .calculo_pedido import CalculoPedidoService
 from .calculo_sistemas import CalculoSistemasService
 from .calculo_tracao import CalculoTracaoService
 from .dimensionamento import DimensionamentoService
@@ -9,7 +10,6 @@ from .pricing import PricingService
 __all__ = [
     "CalculoCabineService",
     "CalculoCarrinhoService",
-    "ComponentesCalculoService",
     "CalculoPedidoService",
     "CalculoSistemasService",
     "CalculoTracaoService",
--- fuza_elevadores/settings.py
+++ fuza_elevadores/settings.py
@@ -91,6 +91,7 @@ INSTALLED_APPS = [
 # Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
